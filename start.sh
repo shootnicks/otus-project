4f66a1c9608d02ae8cdb1ad011835b1d6c20e2e3
Andriyanov Pavel <pg.andriyanov@gmail.com>
2024-06-11 13:13:11 +0300

    Исправил удаление образов.

--- start.sh
+++ start.sh
@@ -40,25 +40,15 @@ echo "---\e[1;m"
 mkdir -p docker_images
 mount -t nfs 192.168.1.202:/mnt/otus-project/docker_images/ ./docker_images
 docker load -i docker_images/nginx.tar;
-rm docker_images/nginx.tar;
 docker load -i docker_images/httpd.tar;
-rm docker_images/httpd.tar;
 docker load -i docker_images/mysql.tar;
-rm docker_images/mysql.tar;
 docker load -i docker_images/zabbix-server-mysql.tar;
-rm docker_images/zabbix-server-mysql.tar;
 docker load -i docker_images/zabbix-web-nginx-mysql.tar;
-rm docker_images/zabbix-web-nginx-mysql.tar;
 docker load -i docker_images/zabbix-agent.tar;
-rm docker_images/zabbix-agent.tar;
 docker load -i docker_images/ELKB/elasticsearch.8.13.4.tar;
-rm docker_images/ELKB/elasticsearch.8.13.4.tar;
 docker load -i docker_images/ELKB/logstash.8.13.4.tar;
-rm docker_images/ELKB/logstash.8.13.4.tar;
 docker load -i docker_images/ELKB/kibana.8.13.4.tar;
-rm docker_images/ELKB/kibana.8.13.4.tar;
 docker load -i docker_images/ELKB/filebeat.8.13.4.tar;
-rm docker_images/ELKB/filebeat.8.13.4.tar;
 umount backup_zabbix
 rm -r docker_images;
 
